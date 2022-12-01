1ed00b7db3918f724611c4ffedb37fceb4fcac53
jahway603 <jahway603@protonmail.com>
2022-12-01 11:47:03 -0500

    minor changes to build scripts

--- util/build.sh
+++ util/build.sh
@@ -3,10 +3,10 @@
 # Distributed under the GPLv3 software license, see the accompanying
 # file LICENSE or https://www.gnu.org/licenses/gpl-3.0.en.html
 # Purpose: Script to build Hush lightwalletd on x86 64-bit arch
-# Check if go is installed on system and exits if it is not
 
 set -e
 
+# Check if go is installed on system and exits if it is not
 if ! [ -x "$(command -v go)" ]; then
   echo 'Error: go is not installed. Install go and try again.' >&2
   exit 1
@@ -25,9 +25,7 @@ echo "+------'+------'+------'+------'+------'+------'+------'+------'+------'+-
 # now to compiling...
 echo ""
 echo "You have go installed, so starting to compile Hush lightwalletd for you..."
-# cd `pwd`/cmd/server
 go build -o lightwalletd main.go
-# mv lightwalletd `pwd`/../../lightwalletd
 echo ""
 echo "Hush lightwalletd is now compiled for you. Enjoy and reach out if you need support."
 echo "For options, run ./lightwalletd --help"
--- util/build_arm.sh
+++ util/build_arm.sh
@@ -2,9 +2,10 @@
 # Copyright 2021 The Hush Developers
 # Distributed under the GPLv3 software license, see the accompanying
 # file LICENSE or https://www.gnu.org/licenses/gpl-3.0.en.html
-#
 # Purpose: Script to build the ARM or aarch64 architecture,
-#		which is what the PinePhone boards are running
+# which is what the PinePhone & newer Raspberry Pi boards are running
+
+set -e
 
 # Check if go is installed on system and exits if it is not
 if ! [ -x "$(command -v go)" ]; then
@@ -25,10 +26,8 @@ echo "+------'+------'+------'+------'+------'+------'+------'+------'+------'+-
 # now to compiling...
 echo ""
 echo "You have go installed, so starting to compile Hush lightwalletd for you..."
-cd `pwd`/cmd/server
 env GOOS=linux GOARCH=arm64 go build -o lightwalletd_aarch64 main.go
 mv lightwalletd_aarch64 lightwalletd
-mv lightwalletd `pwd`/../../lightwalletd
 echo ""
 echo "Hush lightwalletd is now compiled for your ARM farm or Pine Rock64. Enjoy and reach out if you need support."
 echo "For options, run ./lightwalletd --help"
